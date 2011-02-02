74606a05b8fe97d67bdb137723133b35a29ac190
Mikin Faldu <Mikin@Mikins-MacBook.local>
2011-02-01 18:15:25 -0800

    - fixed a paginate syntax that caused dashboard page to load very slow! - optimize a sql

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,10 +11,10 @@ class DashboardController < ApplicationController
   def index
     closed_status = Status.find_by_name("Closed")
     
-    @articles_no_reply = Article.where(:replies => 0).sort(:link_time.desc, :id.desc).all.paginate(:page => params[:page])
+    @articles_no_reply = Article.where(:replies.lt => 1).sort(:link_time.desc, :id.desc).paginate(:page => params[:page])
     @articles_no_reply.delete_if { |a| a.status_id == closed_status.id }
 
-    @articles = Article.where(:replies.ne => 0).sort(:link_time.desc, :id.desc).all.paginate(:page => params[:page])
+    @articles = Article.where(:replies.gte => 1).sort(:link_time.desc, :id.desc).paginate(:page => params[:page])
     @articles.delete_if { |a| a.status_id == closed_status.id }
 
     @closed_articles = Article.where(:status_id => closed_status.id).sort(:link_time.desc, :id.desc).all
@@ -33,10 +33,10 @@ class DashboardController < ApplicationController
     @user = User.find_by_id(user_id)
     closed_status = Status.find_by_name("Closed")      
 
-    @articles_no_reply = Article.where({:replies => 0, :user_id => user_id}).sort(:link_time.desc, :id.desc).all
+    @articles_no_reply = Article.where({:replies.lt => 1, :user_id => user_id}).sort(:link_time.desc, :id.desc).all
     @articles_no_reply.delete_if { |a| a.status_id == closed_status.id }
 
-    @articles = Article.where({:replies.ne => 0, :user_id => user_id}).sort(:link_time.desc).all
+    @articles = Article.where({:replies.gte => 1, :user_id => user_id}).sort(:link_time.desc).all
     @articles.delete_if { |a| a.status_id == closed_status.id }
 
     @closed_articles = Article.where(:user_id => user_id, :status_id => closed_status.id).sort(:link_time.desc, :id.desc).all
